e8d18ee78799144d6bdd83082e7ea3cfba058eae
Yiyang LI <yyli@ezesoft.com>
2015-04-21 15:01:29 -0500

    complete the HW1 in Scala, havent tested yet.

--- ProgLang/HW1/HW1.scala
+++ ProgLang/HW1/HW1.scala
@@ -10,14 +10,14 @@ object HW1_Scala {
   def isDateEqual(first: (Int, Int, Int), second: (Int, Int, Int)) : Boolean =
   	(first._1 == second._1 && first._2 == second._2 && first._3 == second._3)
                                                   //> isDateEqual: (first: (Int, Int, Int), second: (Int, Int, Int))Boolean
-  def isDateOlder(first: (Int, Int, Int), second: (Int, Int, Int)) : Boolean =
+  def is_older(first: (Int, Int, Int), second: (Int, Int, Int)) : Boolean =
   	if (first._1 == second._1)
   		if (first._2 == second._2)
   			if (first._3 == second._3)
   				false
   				else (first._3 < second._3)
   		else (first._2 < second._2)
-  	else (first._1 < second._1)               //> isDateOlder: (first: (Int, Int, Int), second: (Int, Int, Int))Boolean
+  	else (first._1 < second._1)               //> is_older: (first: (Int, Int, Int), second: (Int, Int, Int))Boolean
   def number_in_month(dates: List[(Int, Int, Int)], month: Int) : Int = dates match {
   	case Nil => 0
   	case date :: tail => (if (date._2 == month) 1 else 0) + number_in_month(tail, month)
@@ -52,4 +52,30 @@ object HW1_Scala {
   def date_to_string(date: (Int, Int, Int)) : String = {
   	get_nth(MONTHS, date._2) + date._3.toString + date._1.toString
   }                                               //> date_to_string: (date: (Int, Int, Int))String
+  def number_before_reaching_sum(sum: Int, lst: List[Int]) : Int = {
+  	if (lst.isEmpty)
+  		throw new java.lang.IllegalArgumentException("Sum of the elements can't reach the sum")
+  	else
+  		if (sum - lst.head > 0)
+  			1 + number_before_reaching_sum(sum - lst.head, lst.tail)
+  		else
+  		  0
+  }                                               //> number_before_reaching_sum: (sum: Int, lst: List[Int])Int
+  def what_month(day: Int) : Int = {
+  	number_before_reaching_sum(day, DAYS)
+  }                                               //> what_month: (day: Int)Int
+  def month_range(day1: Int, day2: Int) : List[Int] = {
+  	if (day1 <= day2)
+  		what_month(day1) :: month_range(day1 + 1, day2)
+  	else
+ 			Nil
+  }                                               //> month_range: (day1: Int, day2: Int)List[Int]
+  def oldest(dates: List[(Int, Int, Int)]) : Option[(Int, Int, Int)] = dates match{
+  	case Nil => None
+  	case date1 :: tail => oldest(tail) match { case None => Some(date1)
+  												 										 case Some(tailOldest) => (if (is_older(date1, tailOldest))
+   														 																							Some(date1)
+  													 																						 else
+  														 																					    Some(tailOldest))}
+  }                                               //> oldest: (dates: List[(Int, Int, Int)])Option[(Int, Int, Int)]
 }
